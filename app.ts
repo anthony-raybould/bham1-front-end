be681541608b36d0a3c38b0575ddd72b5901d6bc
Anthony Raybould <anthony.raybould@kainos.com>
2023-09-12 11:03:26 +0100

    Explicitly defined express-session values.

--- app.ts
+++ app.ts
@@ -25,7 +25,13 @@ app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}))
 
-app.use(expressSession({secret : "NOT HARDCODED SECRET", cookie : {maxAge : 60000}}))
+app.use(expressSession({
+    secret : "NOT HARDCODED SECRET",
+    resave : true,
+    saveUninitialized : true,
+    cookie : {maxAge : 60000},
+
+}))
 
 declare module "express-session" {
     interface SessionData {
