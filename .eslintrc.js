460014a227232394e99cfaf2e747e5719569f315
Manpreet Singh Mandair <manpreet.mandair@kainos.com>
2023-09-13 11:13:05 +0100

    US038-Add-a-linting-check

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "max-len": ["error", { "code": 100 }], //enforce a max line length
+        "indent": ["error", 4], //enforce two spaces for indentation
+        "no-unused-vars": "error" //no unused variables 
+    }
+}
