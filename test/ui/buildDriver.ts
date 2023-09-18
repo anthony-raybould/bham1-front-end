da8b0afc9dda1975523ef7866494dbb4dd43c2a0
Anthony Raybould <anthony.raybould@kainos.com>
2023-09-18 16:56:05 +0100

    Set UI test browser size to 1920x1080.

--- test/ui/buildDriver.ts
+++ test/ui/buildDriver.ts
@@ -5,6 +5,6 @@ export const buildDriver = function() {
 
     return new webdriver.Builder()
       .withCapabilities(webdriver.Capabilities.chrome())
-      .setChromeOptions(new chrome.Options().addArguments('--headless'))
+      .setChromeOptions(new chrome.Options().addArguments('--headless', "--window-size=1920,1080"))
       .build();
 } 
