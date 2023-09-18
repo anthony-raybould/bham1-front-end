65b274f9c011671f902102d16214d09174704c53
Anthony Raybould <anthony.raybould@kainos.com>
2023-09-18 10:59:33 +0100

    Fixed accessibility testing URLs.

--- test/accessibility/indexTests.ts
+++ test/accessibility/indexTests.ts
@@ -2,13 +2,11 @@
 import pa11y from "pa11y";
 import { processResults } from "./processResults";
 
-const baseUrl = "http://localhost:3000";
-
 describe('Index Page Accessibility Tests', async () => {
 
   it('Index page', async () => {
 
-    const results = await pa11y(baseUrl + "/");
+    const results = await pa11y(process.env.UI_TEST_URL + "/");
     processResults(results);
 
   });
--- test/accessibility/loginTests.ts
+++ test/accessibility/loginTests.ts
@@ -2,20 +2,19 @@
 import pa11y from "pa11y";
 import { processResults } from "./processResults";
 
-const baseUrl = "http://localhost:3000";
 
 describe('Login Page Accessibility Tests', async () => {
 
   it('Login form', async () => {
 
-    const results = await pa11y(baseUrl + "/login");
+    const results = await pa11y(process.env.UI_TEST_URL + "/login");
     processResults(results);
 
   });
 
   it('Login form invalid credentials', async () => {
 
-    const results = await pa11y(baseUrl + "/login", {
+    const results = await pa11y(process.env.UI_TEST_URL + "/login", {
       actions: [
         "set field #email to abc",
         "set field #password to xyz",
