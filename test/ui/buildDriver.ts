3ec48cd0a29f2bc13eddb6ffb76b5ddccdd776dd
Anthony Raybould <anthony.raybould@kainos.com>
2023-09-19 12:11:14 +0100

    Changed how headless browser is created.

--- test/ui/buildDriver.ts
+++ test/ui/buildDriver.ts
@@ -5,10 +5,7 @@ export const buildDriver = function() {
 
     const driver = new Builder()
       .withCapabilities(Capabilities.chrome())
-      .setChromeOptions(new Options().addArguments(
-        '--headless',
-        "--window-size=1920,1080", 
-        "--start-maximized"))
+      .setChromeOptions(new Options().headless())
       .build();
 
       /* Enforces a minimum wait before failing when getting elements, 
