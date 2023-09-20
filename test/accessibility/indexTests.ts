bac1a058cb681e5a691276ec1dedc73c6ca044e1
Anthony Raybould <anthony.raybould@kainos.com>
2023-09-20 09:52:46 +0100

    Rewrote an accessibility test.

--- test/accessibility/indexTests.ts
+++ test/accessibility/indexTests.ts
@@ -1,13 +1,21 @@
 
 import pa11y from "pa11y";
 import { processResults } from "./processResults";
+import { assert } from "chai";
+const _chai = require('chai');
 
 describe('Index Page Accessibility Tests', async () => {
 
   it('Index page', async () => {
 
-    const results = await pa11y(process.env.UI_TEST_URL + "/");
-    processResults(results);
+    try {
+      const results = await pa11y(process.env.UI_TEST_URL + "/");
+      processResults(results);
+
+    } catch (e) {
+      assert.fail(e);
+    }
+    
 
   });
 
