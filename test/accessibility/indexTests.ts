ce3f3758dfbff27cac256d45a1d4d4bf77fdec00
Anthony Raybould <anthony.raybould@kainos.com>
2023-09-20 10:05:59 +0100

    Test Pa11y command line.

--- test/accessibility/indexTests.ts
+++ test/accessibility/indexTests.ts
@@ -1,22 +1,16 @@
 
 import pa11y from "pa11y";
 import { processResults } from "./processResults";
-import { assert } from "chai";
+import {it} from "mocha"
 const _chai = require('chai');
 
 describe('Index Page Accessibility Tests', async () => {
 
   it('Index page', async () => {
 
-    try {
-      const results = await pa11y(process.env.UI_TEST_URL + "/");
-      processResults(results);
-
-    } catch (e) {
-      assert.fail(e);
-    }
+    const results = await pa11y(process.env.UI_TEST_URL + "/");
+    processResults(results);
     
-
   });
 
 });
