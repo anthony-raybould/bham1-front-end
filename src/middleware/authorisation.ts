c8a5c3a7da5cb945220b8cef3755fdffb24dee6f
Leonardo Bishop <me@leonardobishop.com>
2023-09-20 16:15:37 +0100

    Remove nested if from requireRole middleware

--- src/middleware/authorisation.ts
+++ src/middleware/authorisation.ts
@@ -3,15 +3,15 @@ import { Request, Response, NextFunction } from "express";
 export function requireRole(requiredRole: string) {
     return function (req: Request, res: Response, next: NextFunction) {
         if (req.session && (req.session.user?.role == 'Admin' || req.session.user?.role === requiredRole)) {
-            next();
+            return next();
+        } 
+
+        if (req.session.user) {
+            res.locals.errorMessage = `User ${req.session.user?.email} is not authorized to view this page.`;
         } else {
-            if (req.session.user) {
-                res.locals.errorMessage = `User ${req.session.user?.email} is not authorized to view this page.`;
-            } else {
-                res.locals.errorMessage = `You must be logged in to view this page.`;
-            }
-            res.render('forbidden');
+            res.locals.errorMessage = `You must be logged in to view this page.`;
         }
+        res.render('forbidden');
     }
 }
 
