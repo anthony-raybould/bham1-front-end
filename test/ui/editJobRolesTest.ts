51925adcd04bca2c3e8d6f1dc6af89e2a184eafe
robbiecrabtree <robbie.crabtree@kainos.com>
2023-09-21 15:26:03 +0100

    edit job role uI and unit tests

--- test/ui/editJobRolesTest.ts
+++ test/ui/editJobRolesTest.ts
@@ -8,10 +8,52 @@ describe('job-roles page', () => {
         const driver = new webdriver.Builder().forBrowser('chrome').build();
 
         await driver.get(process.env.UI_TEST_URL + '/job-roles/edit/1');
+        const pageTitle = await driver.findElement(By.css('h2')).getText();
+        expect(pageTitle).to.equal('Edit Job Role');
+    
+        const jobRoleNameInput = await driver.findElement(By.id('jobRoleName'));
+        const bandSelect = await driver.findElement(By.id('band')); 
+        const capabilitySelect = await driver.findElement(By.id('capability'));
+        const jobSpecSummaryInput = await driver.findElement(By.id('jobSpecSummary'));
+        const responsibilitiesInput = await driver.findElement(By.id('responsibilities'));
+        const sharePointInput = await driver.findElement(By.id('sharePoint'));
+        const saveButton = await driver.findElement(By.css('button[type="submit"]'));
+    
+        expect(jobRoleNameInput).to.exist;
+        expect(bandSelect).to.exist;
+        expect(capabilitySelect).to.exist;
+        expect(jobSpecSummaryInput).to.exist;
+        expect(responsibilitiesInput).to.exist;
+        expect(sharePointInput).to.exist;
+        expect(saveButton).to.exist;
 
-        const jobRole = await driver.findElement(By.id('job-roles-table'));
-        expect(jobRole).to.not.be.null;
-        
         await driver.quit();
-    })    
+      });
+    
+    //   it('should submit the form', async function () {
+    //     // Navigate to the edit job role page
+    //     await driver.get('http://your-website.com/job-roles/edit/1'); // Replace with your actual URL
+    
+    //     // Fill out the form fields
+    //     const jobRoleNameInput = await driver.findElement(By.id('jobRoleName'));
+    //     const bandSelect = await driver.findElement(By.id('band'));
+    //     const capabilitySelect = await driver.findElement(By.id('capability'));
+    //     const jobSpecSummaryInput = await driver.findElement(By.id('jobSpecSummary'));
+    //     const responsibilitiesInput = await driver.findElement(By.id('responsibilities'));
+    //     const sharePointInput = await driver.findElement(By.id('sharePoint'));
+    //     const saveButton = await driver.findElement(By.css('button[type="submit"]'));
+    
+    //     await jobRoleNameInput.sendKeys('Updated Role');
+    //     await bandSelect.sendKeys('Updated Band');
+    //     await capabilitySelect.sendKeys('Updated Capability');
+    //     await jobSpecSummaryInput.sendKeys('Updated Summary');
+    //     await responsibilitiesInput.sendKeys('Updated Responsibilities');
+    //     await sharePointInput.sendKeys('Updated SharePoint');
+    
+    //     // Submit the form
+    //     await saveButton.click();
+    
+    //     // You can add assertions here to verify that the form was submitted successfully
+    //     // For example, you can check for a success message or navigate to a confirmation page.
+    //   });
 });
