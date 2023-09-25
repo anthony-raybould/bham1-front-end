a587a250623477ab64ac4e1214aca6bc5e09f882
Anthony Raybould <anthony.raybould@kainos.com>
2023-09-25 11:01:30 +0100

    Wrote unit tests for job role matrix.

--- test/unit/controller/jobRoleControllerTest.ts
+++ test/unit/controller/jobRoleControllerTest.ts
@@ -6,6 +6,7 @@ import { jobRoleService } from '../../../src/service/jobRoleService';
 import router from '../../../src/router';
 import { bandService } from '../../../src/service/bandService';
 import { capabilityService } from '../../../src/service/capabilityService';
+import { JobRole } from '../../../src/model/jobRole';
 const expect = chai.expect;
 
 const app = express();
@@ -119,6 +120,103 @@ describe('jobRoleController', () => {
                 expect(res.text).to.contain('Test band');
                 expect(res.text).to.contain('Text capability')
                 expect(res.text).to.contain('Test job role')
+            }
+        );
+    });
+
+    it('should render job-role-matrix.html template with no job roles', async () => {
+        sinon.stub(jobRoleService, 'getJobRoleMatrix').resolves({
+            bands: [{bandID: 6, bandName: "Associate"}, {bandID: 5, bandName: "Trainee"}],
+            capabilities: [{capabilityID: 1, capabilityName: "Engineering"}, {capabilityID: 2, capabilityName: "Cyber Security"}],
+            jobRolesGrid: [[[], []], [[], []]]
+        });
+
+        request(app)
+            .get('/job-roles/matrix')
+            .expect(200)
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .end((err, res) => {
+                expect(res.text).to.contain("<h1>Job Roles Matrix</h1>");
+                expect(res.text).to.contain("Engineering");
+                expect(res.text).to.contain("Trainee")
+            }
+        );
+    });
+
+    it('should render job-role-matrix.html template with no bands', async () => {
+        sinon.stub(jobRoleService, 'getJobRoleMatrix').resolves({
+            bands: [],
+            capabilities: [{capabilityID: 1, capabilityName: "Engineering"}],
+            jobRolesGrid: []
+        });
+
+        request(app)
+            .get('/job-roles/matrix')
+            .expect(200)
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .end((err, res) => {
+                expect(res.text).to.contain("<h5>There are either no bands or no capabilities, thus matrix cannot be displayed.</h5>");
+            }
+        );
+    });
+
+    it('should render job-role-matrix.html template with no capabilities', async () => {
+        sinon.stub(jobRoleService, 'getJobRoleMatrix').resolves({
+            bands: [{bandID: 6, bandName: "Associate"}],
+            capabilities: [],
+            jobRolesGrid: [[]]
+        });
+
+        request(app)
+            .get('/job-roles/matrix')
+            .expect(200)
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .end((err, res) => {
+                expect(res.text).to.contain("<h5>There are either no bands or no capabilities, thus matrix cannot be displayed.</h5>")
+            }
+        );
+    });
+
+    it('should render job-role-matrix.html template with job roles', async () => {
+
+        const associateBand = {bandID: 6, bandName: "Associate"}
+        const engineeringCapability = {capabilityID: 1, capabilityName: "Engineering"}
+        const jobRole : JobRole = {
+            jobRoleID: 9, 
+            jobRoleName: "Associate Software Engineer", 
+            jobSpecSummary: "", 
+            band: associateBand, 
+            capability: engineeringCapability,
+            responsibilities: "",
+            sharePoint: ""
+        }
+
+        sinon.stub(jobRoleService, 'getJobRoleMatrix').resolves({
+            bands: [associateBand, {bandID: 5, bandName: "Trainee"}],
+            capabilities: [engineeringCapability, {capabilityID: 2, capabilityName: "Cyber Security"}],
+            jobRolesGrid: [[[jobRole], []], [[], []]]
+        });
+
+        request(app)
+            .get('/job-roles/matrix')
+            .expect(200)
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .end((err, res) => {
+                expect(res.text).to.contain(jobRole.jobRoleName);
+            }
+        );
+    });
+
+    it('should render job-role-matrix.html template with error message', async () => {
+        sinon.stub(jobRoleService, 'getJobRoles').throws(new Error('Test error'));
+
+        request(app)
+            .get('/job-roles/matrix')
+            .expect(200)
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .end((err, res) => {
+                expect(res.text).to.contain("<h1>Job Roles Matrix</h1>");
+                expect(res.text).to.contain('Test error');
             }
         );
     });
